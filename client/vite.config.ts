a549daeeacee21bd57f8fe02735d02384300e5af
MystProgrammer <animeshdeb89@gmail.com>
2025-09-15 15:57:20 -0400

    fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
-  base: process.env.VITE_BASE_PATH || "/moodAudio",
+  base: process.env.VITE_BASE_PATH || "/",
   resolve:{
     alias:{
       "@":path.resolve(__dirname,'./src'),
